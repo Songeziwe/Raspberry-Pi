dc990959ad6e4f66dfaffb5fda352580a5b4884c
Songeziwe S. Soboois <sobooissongeziwe@gmail.com>
2019-07-28 23:39:17 +0100

    Able to blink LED using a button

--- template.py
+++ template.py
@@ -16,33 +16,44 @@ from time import sleep
 # List of pins used from the RPI:
 # Pin 6 - Ground
 # Pin 7 - LED0
-# Pin 12- LED1
-# Pin 11- LED2
-# Pin 13- Black button
-# Pin 15- Red button
+# Pin 13- LED1
+# Pin 15- LED2
+# Pin 11- Black button : increment
+# Pin 12- Red button   : decrement
 
 # Global variables
-LEDS    = (7, 12, 11)
-BUTTONS = (13, 15)
+LEDS    = (7, 13, 15)
+BUTTONS = (11, 12)
 
 # Function for initializing the PI
 def init_GPIO():
+    
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(LEDS, GPIO.OUT)
-    # have to setup the buttons
+    GPIO.setup(LEDS[0], GPIO.OUT)
+    GPIO.setup(LEDS[1], GPIO.OUT)
+    GPIO.setup(LEDS[2], GPIO.OUT)
 
-def light_LED():
-    GPIO.output(LEDS, GPIO.HIGH)
+    GPIO.setup(BUTTONS[0], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(BUTTONS[1], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+    # Ensure that all LEDs are off initially
+    GPIO.output(LEDS, [0, 0, 0])
+
+# Function for blinking LEDs
+def light_LED(LED_channel):
+    GPIO.output(LED_channel, True)
     sleep(1)
-    GPIO.output(LEDS, GPIO.LOW)
+    GPIO.setup(LED_channel, False)
     sleep(1)
 
 def main():
-    light_LED()
+    if GPIO.input(BUTTONS[0]):
+        light_LED(LEDS[2])
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
+    GPIO.setwarnings(False)
     init_GPIO()
     try:
         while True:
