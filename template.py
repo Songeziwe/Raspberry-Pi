58123161f918a9b3c614f49b7b78468f2d79bedb
Songeziwe S. Soboois <sobooissongeziwe@gmail.com>
2019-07-29 13:44:06 +0100

    Implemented two buttons for incrementing and decrementing the binary counter and a method for displaying the binary counter to the LEDs

--- template.py
+++ template.py
@@ -24,10 +24,10 @@ from time import sleep
 # Global variables
 LEDS    = (7, 13, 15)
 BUTTONS = (11, 12)
+counter = 0
 
 # Function for initializing the PI
 def init_GPIO():
-    
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(LEDS[0], GPIO.OUT)
     GPIO.setup(LEDS[1], GPIO.OUT)
@@ -38,16 +38,58 @@ def init_GPIO():
 
     # Ensure that all LEDs are off initially
     GPIO.output(LEDS, [0, 0, 0])
-    GPIO.add_event_detect(BUTTONS[0], GPIO.RISING, light_LED)
+    GPIO.add_event_detect(BUTTONS[0], GPIO.RISING, callback=increment, bouncetime=300)
+    GPIO.add_event_detect(BUTTONS[1], GPIO.RISING, callback=decrement, bouncetime=300)
+
+# Increment the counter
+# This method will be executed when an interrupt occures
+# event - pin number of the clicked button
+def increment(event):
+    global counter
+    counter += 1
+    if counter > 7:
+        counter = 0
+
+# Decrement the counter
+# This Function will be executed when an interrupt occures
+# event - pin number of the clicked button
+def decrement(event):
+    global counter
+    counter -= 1
+    if counter < 0:
+        counter = 7
+
+# convert counter to Binary
+# value  - number to be converted to binary
+# return - a list of 0's and/or 1's representing the value 
+def convert2Binary(value):
+    binaryList = []
+    result = value
+    while True:
+        remainder = result % 2
+        result = result // 2
+        if remainder == 0:
+            binaryList.append(0)
+        else:
+            binaryList.append(1)
+
+        if result <= 0:
+            break
+    if len(binaryList) < 2:
+        binaryList.append(0)
+        binaryList.append(0)
+    elif len(binaryList) < 3:
+        binaryList.append(0)
+    return binaryList
+
 # Function for blinking LEDs
-def light_LED(event):
-    GPIO.output(LEDS, [1, 1, 1])
-    sleep(0.1)
-    GPIO.output(LEDS, [0, 0, 0])
+def light_LED():
+    binary = convert2Binary(counter)
+    GPIO.output(LEDS, binary)
 
 def main():
-    #GPIO.add_event_detect(BUTTONS[0], GPIO.RISING, light_LED)
-    a = 0
+   light_LED()
+ 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     GPIO.setwarnings(False)
